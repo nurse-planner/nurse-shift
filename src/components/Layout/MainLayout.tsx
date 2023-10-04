cd3a07911ceb8c2a173c64ac1bb96ce60c22828f
LireEruel <jsh001505@gmail.com>
2023-10-05 00:36:16 +0900

    header 수정

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -4,15 +4,21 @@ import { Content, Footer, Header } from "antd/es/layout/layout";
 import { theme } from "antd";
 import "@/assets/boilerplate.css";
 import "@/assets/App.scss";
+import logo from "@/assets/logo.svg";
+import { useNavigate } from "react-router-dom";
 
 type MainLayoutProps = {
   children: React.ReactNode;
 };
 
 export const MainLayout = ({ children }: MainLayoutProps) => {
+  const navigate = useNavigate();
   const {
     token: { colorBgContainer },
   } = theme.useToken();
+  const goHome = () => {
+    navigate("/nurse-shift");
+  };
 
   return (
     <div className="App">
@@ -23,7 +29,13 @@ export const MainLayout = ({ children }: MainLayoutProps) => {
             className="header"
             style={{ padding: 0, background: colorBgContainer }}
           >
-            <h1>Nurse planner</h1>
+            <div
+              onClick={goHome}
+              aria-label="logo"
+              className="flex space-x-2 items-center cursor-pointer"
+            >
+              <img className="h-8 ml-4 w-auto" src={logo} alt="Workflow" />
+            </div>
           </Header>
           <Content style={{ margin: "0 16px" }}>{children}</Content>
           <Footer style={{ textAlign: "center" }}>
